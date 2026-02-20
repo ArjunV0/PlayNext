16ceff7fd332e05f1af51661504293ae29ea8bda
Arjun V <arjun.v@wednesday.is>
2026-02-20 18:29:45 +0530

    style: Fix prettier formatting in SearchPage

--- features/search/SearchPage.tsx
+++ features/search/SearchPage.tsx
@@ -8,11 +8,7 @@ export function SearchPage() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold text-gray-900 dark:text-white">Search</h1>
-      {query && (
-        <p className="mt-2 text-gray-500 dark:text-gray-400">
-          Results for &ldquo;{query}&rdquo;
-        </p>
-      )}
+      {query && <p className="mt-2 text-gray-500 dark:text-gray-400">Results for &ldquo;{query}&rdquo;</p>}
     </div>
   )
 }
